3e7db81c4494c74cd87ab571ccae0a07fe958114
dubeyRahul26 <153608907+dubeyRahul26@users.noreply.github.com>
2025-02-11 00:38:53 +0530

    updated

--- src/components/navbar/NavbarMain.jsx
+++ src/components/navbar/NavbarMain.jsx
@@ -1,8 +1,8 @@
 import { GiHamburgerMenu } from "react-icons/gi";
 import React from "react";
-import NavbarLogo from "./NavBarLogo";
-import NavbarLink from "./NavbarLink";
-import NavbarBtn from "./NavbarBtn";
+import NavBarLogo from "./NavBarLogo";
+import NavBarLink from "./NavbarLink";
+import NavBarBtn from "./NavbarBtn";
 import { useState } from "react";
 
 const NavbarMain = () => {
@@ -15,12 +15,12 @@ const NavbarMain = () => {
   return (
     <nav className="max-w-[1300px] mx-auto px-6 w-full fixed left-[50%] -translate-x-[50%] z-20 flex gap-4 mt-2 ">
       <div className="flex justify-between w-full max-w-[1200px] mx-auto bg-[#34495E] items-center py-1 px-4 rounded-r-full rounded-l-full border-[0.5px] border-[#BDC3C7]">
-        <NavbarLogo />
+        <NavBarLogo />
         <div className="flex items-center gap-5">
           <div className={`${menuOpen ? "block" : "hidden"} lg:block`}>
-            <NavbarLink />
+            <NavBarLink />
           </div>
-        <NavbarBtn />
+        <NavBarBtn />
         </div>
       </div>
       <div className="flex lg:hidden p-4 items-center justify-center rounded-full border border-cyan-300 bg-black">
